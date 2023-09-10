fdaf745a92b9364c8959ac944c145326a2a3fcd3
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-10 09:01:44 -0700

    My file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,13 +6,15 @@
 int main(void)
 {
 	int n;
+
 	for (n = 0; n < 16; n++)
 	{
-		if ( n < 10)
+		if (n < 10)
 		{
 		putchar('0' + n);
 		}
-		else {
+		else
+		{
 			putchar('a' + n - 10);
 		}
 	}
