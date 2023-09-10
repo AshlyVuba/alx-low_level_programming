71bb74dab0536ee30b7c208365ebc98e4dfa1694
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-10 07:58:44 -0700

    My file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Enty point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lovercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(upercase);
+		uppercase++;
+	}
+	putchar('\n');
+	return (0);
+}
