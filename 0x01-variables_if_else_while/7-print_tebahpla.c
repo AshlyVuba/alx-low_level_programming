4c5c3af9014243ae616f251a225e75e1b7cc2333
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-10 08:42:46 -0700

    My file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char letter ='z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	retuen (0);
+}
