d4b7389df15c76d0777de912dfd07840ea45d6c0
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-11 10:50:33 -0700

    My file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 10; a <= 99; a++)
+	{
+		for (b = 10; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 99 || b != 99);
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
