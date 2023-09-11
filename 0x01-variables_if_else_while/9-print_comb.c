74f210a556ca9d29e15cb3d91173caa189b0f454
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-11 07:13:24 -0700

    My file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,11 @@
  */
 int main(void)
 {
-	int a, b;
+	int a;
 
 	for (a = 0; a <= 9; a++)
 	{
 		putchar(a + '0');
-	}
-	for (b = 0; b <= 9; b++)
-	{
-		putchar(b + '0');
 		putchar(',');
 	}
 	return (0);
