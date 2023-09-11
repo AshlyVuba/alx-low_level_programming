e182d31442acfe6bd42160111defb8ffabe0e5bb
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-11 10:51:54 -0700

    My file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 			putchar((a % 10) + '0');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
-			if (a != 99 || b != 99);
+			if (a != 99 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
