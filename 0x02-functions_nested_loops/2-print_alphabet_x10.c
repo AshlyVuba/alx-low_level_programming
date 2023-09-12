50e18d23356797bf588b2fe46549977f6b35eaff
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-12 02:03:49 -0700

    My commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		char alpha = 'a';
+		while (alpha <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
