4396afd9a0f7fa083c123958ab20319fb12c1a93
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-18 10:12:17 +0200

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,25 +3,23 @@
 #include <time.h>
 #include <string.h>
 
-// Function to generate a random character
 char getRandomChar() {
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:'\",.<>?";
     const size_t charsetSize = sizeof(charset) - 1;
     return charset[rand() % charsetSize];
 }
 
-// Function to generate a random password
 void generateRandomPassword(char *password, int length) {
     srand(time(NULL));
     for (int i = 0; i < length; i++) {
         password[i] = getRandomChar();
     }
-    password[length] = '\0'; // Null-terminate the string
+    password[length] = '\0';
 }
 
 int main() {
-    int passwordLength = 12; // Change this to set the desired password length
-    char password[passwordLength + 1]; // +1 for the null terminator
+    int passwordLength = 12; 
+    char password[passwordLength + 1];
 
     generateRandomPassword(password, passwordLength);
 
