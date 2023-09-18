ca26e139ff1f0f68984036b57bce511897978618
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-17 23:55:34 -0700

    My commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
-<<<<<<< HEAD
-#define MAIN_h
-
-int putchar _(char c);
-=======
 #define MAAIN_H
 
 int _putchar(char c);
->>>>>>> ed37f02e5fa72ed55c510036e0377f456d0edaf8
 void reset_to_98(int *n);
 void swap_int(int *a,int *b);
 int _strlen(char *s);
