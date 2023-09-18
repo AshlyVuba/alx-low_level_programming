0205624c978a57c040a34c25730e9ed60732a51d
AshlyVuba <aphiwe.avuba@gmail.com>
2023-09-18 09:48:50 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,12 +3,14 @@
  * rev_string - Reverses a string.
  * @s: The string to be reversed.
  */
-void rev_string(char *s) {
+void rev_string(char *s)
+{
     int length = 0;
     int start = 0;
     int end;
-
-    while (s[length] != '\0') {
+    
+    while (s[length] != '\0')
+    {
         length++;
     }
 
