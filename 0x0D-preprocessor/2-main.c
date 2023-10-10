ac73e64b95f51250c4f0b3144010dce65206f50c
AshlyVuba <aphiwe.avuba@gmail.com>
2023-10-10 03:41:11 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+    *main - Prints the file name of the program
+    * Return: Always zero
+`*/
 int main() {
     printf("Compiled from file: %s\n", __FILE__);
     return 0;
