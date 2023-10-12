7e3c7157400b41ee10b7c711da8f23e021382f9a
AshlyVuba <aphiwe.avuba@gmail.com>
2023-10-12 03:47:23 +0200

    Create 0F-function_pointers 0-print_name.c

---
+++ 0x/0F-function_pointers 0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * print_name - Prints a name
+ * @name: The name to print
+ * @f: A function to perform the printing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
