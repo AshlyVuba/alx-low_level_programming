afe0d9b213d9c7a242d517932338999bf3612972
AshlyVuba <aphiwe.avuba@gmail.com>
2023-11-09 05:09:46 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
   * print_name - Prints a name
   * @name: The name to prints
@@ -9,5 +8,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 		f(name);
 }
