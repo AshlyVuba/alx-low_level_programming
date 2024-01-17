793c955bbe65ef3117a54fb08686bfeb621182e6
AshlyVuba <aphiwe.avuba@gmail.com>
2024-01-17 06:48:51 -0800

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,6 +9,46 @@
  * Return: The int
  */
 
+int _atoi(char *s)
+{
+	int i = 0;
+	int d = 0;
+	int n = 0;
+	int f = 0;
+	int len = 0;
+	int digit = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i +1] > '9')
+			
+			break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return(n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on Success and 1 on Error
+ */
+
 int main(int argc, char *argv[])
 {
 	int num1 = 0;
@@ -16,15 +56,14 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
+		num1 = _atoi(argv[1]);
+		num2 = _atoi(argv[2]);
 		printf("%d\n", num1 * num2);
 	}
 	else
 	{
-		printf("Eroor\n");
+		printf("Error\n");
 		return (1);
 	}
-
-	return (0);
+return (0);
 }
